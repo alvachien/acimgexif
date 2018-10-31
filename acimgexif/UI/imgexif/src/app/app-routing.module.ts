08ea32209a11f4dfc3eb2303b55e71b78333ed58
Alva Chien <alvachien@163.com>
2018-10-31 22:18:11 +0800

    Continues the codes

--- acimgexif/UI/imgexif/src/app/app-routing.module.ts
+++ acimgexif/UI/imgexif/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent } from './home-page';
+
+const routes: Routes = [{
+    path: '',
+    component: HomePageComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- acimgexif/UI/imgexif/src/app/app.module.ts
+++ acimgexif/UI/imgexif/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatBadgeModule, MatCardModule, MatCheckboxModule,
+  MatFormFieldModule, MatSnackBarModule, MatDialogModule, MatInputModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    MatInputModule,
+
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
